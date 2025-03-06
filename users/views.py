ebdc596b9406c7242bc8259c6602ab45c3249cab
Aimurat Torebayev <kastspy@gmail.com>
2025-03-06 08:43:10 +0500

    настраиваем добавление товара в корзину анонимного пользоваетля используя сессии

--- users/views.py
+++ users/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import redirect, render
 from django.urls import reverse
 from traitlets import Instance
 
+from carts.models import Cart
 from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
 
 def login(request):
@@ -14,9 +15,15 @@ def login(request):
             username = request.POST['username']
             password = request.POST['password']
             user = auth.authenticate(username=username, password=password)
+            
+            session_key=request.session.session_key
+            
             if user:
                 auth.login(request, user)
                 messages.success(request, f"{username}, Сучара вошел таки в аккаунт")
+                
+                if session_key:
+                    Cart.objects.filter(session_key=session_key).update(user=user)
                 
                 redirect_page = request.POST.get('next', None)
                 if redirect_page and redirect_page != reverse('user:logout'):
@@ -39,8 +46,15 @@ def registration(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+            
+            session_key=request.session.session_key
+            
             user = form.instance
             auth.login(request, user)
+            
+            if session_key:
+                    Cart.objects.filter(session_key=session_key).update(user=user)
+            
             messages.success(request, f"{user.username}, Ты зарегался бро")
             return HttpResponseRedirect(reverse('main:index'))
     else:
