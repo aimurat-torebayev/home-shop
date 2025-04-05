fb831dcddfa249af735bb5ebf595df6dcd27fcf3
Aimurat Torebayev <kastspy@gmail.com>
2025-04-05 09:34:51 +0500

    переписали на классы авторизацию

--- users/views.py
+++ users/views.py
@@ -1,46 +1,56 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib import auth, messages
+from django.contrib.auth.views import LoginView
 from django.db.models import Prefetch
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from traitlets import Instance
 
 from carts.models import Cart
 from orders.models import Order, OrderItem
 from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
 
-def login(request):
-    if request.method == 'POST':
-        form = UserLoginForm(data=request.POST)
-        if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
-            user = auth.authenticate(username=username, password=password)
-            
-            session_key=request.session.session_key
-            
-            if user:
-                auth.login(request, user)
-                messages.success(request, f"{username}, Сучара вошел таки в аккаунт")
-                
-                if session_key:
-                    Cart.objects.filter(session_key=session_key).update(user=user)
-                
-                redirect_page = request.POST.get('next', None)
-                if redirect_page and redirect_page != reverse('user:logout'):
-                    return HttpResponseRedirect(request.POST.get('next'))
-                
-                return HttpResponseRedirect(reverse('main:index'))
-    else:
-        form = UserLoginForm()
+class UserLoginView(LoginView):
+    template_name = 'users/login.html'
+    form_class = UserLoginForm
+   # success_url = reverse_lazy('main:index')
+   
+    def get_success_url(self):
+        redirect_page = self.request.POST.get('next', None)
+        if redirect_page and redirect_page != reverse('users:logout'):
+            return redirect_page
+        
+        return reverse_lazy('main:index')
     
-    context = {
-        'title': 'Home - Авторизация',
-        'form': form
-    }
+    def form_valid(self, form):
+        session_key = self.request.session.session_key
+        
+        user = form.get_user()
+        
+        if user:
+            auth.login(self.request, user)
+            if session_key:
+                #delete old authorized user carts
+                forgot_carts = Cart.objects.filter(user=user)
+                if forgot_carts.exists():
+                    forgot_carts.delete()
+                    
+                # add new authorized user carts from anonimous session
+                Cart.objects.filter(session_key=session_key).update(user=user)
+                    
+                messages.success(self.request, f"{user.username}, Сучара вошел таки в аккаунт")
+            
+                return HttpResponseRedirect(self.get_success_url())
+        
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Home - Авторизация'
+        return context
     
-    return render(request, 'users/login.html', context)
+    
+
+
 
 def registration(request):
     
@@ -107,3 +117,41 @@ def logout(request):
 
 def users_cart(request):
     return render(request, 'users/users_cart.html')
+
+# def login(request):
+#     if request.method == 'POST':
+#         form = UserLoginForm(data=request.POST)
+#         if form.is_valid():
+#             username = request.POST['username']
+#             password = request.POST['password']
+#             user = auth.authenticate(username=username, password=password)
+            
+#             session_key=request.session.session_key
+            
+#             if user:
+#                 auth.login(request, user)
+#                 messages.success(request, f"{username}, Сучара вошел таки в аккаунт")
+                
+#                 if session_key:
+#                     #delete old authorized user carts
+#                     forgot_carts = Cart.objects.filter(user=user)
+#                     if forgot_carts.exists():
+#                         forgot_carts.delete()
+                    
+#                     # add new authorized user carts from anonimous session
+#                     Cart.objects.filter(session_key=session_key).update(user=user)
+                
+#                 redirect_page = request.POST.get('next', None)
+#                 if redirect_page and redirect_page != reverse('user:logout'):
+#                     return HttpResponseRedirect(request.POST.get('next'))
+                
+#                 return HttpResponseRedirect(reverse('main:index'))
+#     else:
+#         form = UserLoginForm()
+    
+#     context = {
+#         'title': 'Home - Авторизация',
+#         'form': form
+#     }
+    
+#     return render(request, 'users/login.html', context)
